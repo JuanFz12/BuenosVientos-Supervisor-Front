80b54afccc82ac9cc5fb656afa58bcf88c63e60b
Patrick Diestra <patrickdiestra01@gmail.com>
2024-04-08 16:59:10 -0500

    maquetando la pagina de exoneraciones

--- src/pages/exoneraciones/Exoneraciones.jsx
+++ src/pages/exoneraciones/Exoneraciones.jsx
@@ -1,6 +1,10 @@
 import { useEffect } from 'react'
 import { Buscador } from '../../components/buscador/Buscador'
 import { changeDocTitle, titlePages } from '../../consts/titlePage'
+import { useLocation } from 'react-router-dom'
+import { routes } from '../../routes'
+import { BotonNotificacion } from '../../components/botones/BotonNotificacion'
+import { RenderExoneraciones } from './components/RenderExoneraciones'
 
 export function Exoneraciones () {
   useEffect(() => changeDocTitle(titlePages.exoneraciones), [])
@@ -8,28 +12,51 @@ export function Exoneraciones () {
   return (
     <main>
       <Header />
-      exoneraciones
+      <RenderExoneraciones />
     </main>
   )
 }
 
 function Header () {
+  // const nuevaExoneracionModal = useRef()
+
+  const { pathname } = useLocation()
+
   return (
     <header
-      className='w-full flex min-h-9 items-center justify-end mb-8'
+      className='flex justify-between mb-8 flex-wrap gap-8'
     >
-      <section
-        className='flex flex-wrap items-center gap-5'
+      <menu
+        className='flex flex-wrap gap-5 items-center'
       >
-        <button
-          onClick={() => alert('en construccion')}
-          className='w-10 h-9 rounded-lg bg-rojoMarca-100 p-[10px] flex justify-center'
-        >
-          {/* <Filtros fill='#E43530' /> */}
-        </button>
+        <li>
+          <button
+            type='button'
+            // onClick={() => nuevaExoneracionModal.current.showModal()}
+            onClick={() => alert('en construccion')}
+            className='boton-primario-verde w-[200px]'
+          >
+            Nueva Exoneración
+          </button>
+          {/* <NuevaExoneracion refModal={nuevaExoneracionModal} /> */}
+        </li>
 
-        <Buscador />
-      </section>
+        {
+          pathname === routes.exoneraciones && (
+            <li>
+              <BotonNotificacion
+                // to={routes.asistencias.exoneracionesRoutes.solicitudes}
+                onClick={() => alert('en construccion')}
+              />
+            </li>
+          )
+        }
+      </menu>
+
+      <Buscador
+        labelClass='flex-1 max-w-[420px]'
+        className='w-full'
+      />
     </header>
   )
 }
---
+++ src/pages/exoneraciones/components/RenderExoneraciones.jsx
@@ -0,0 +1,114 @@
+import './RenderExoneraciones.css'
+
+import { Etiqueta } from '../../../components/etiquetas/Etiquetas'
+import { ListStyleRow } from '../../../components/listStyle/ListStyleRow'
+import { TablaBase } from '../../../components/tablas/TablaBase'
+import { formatearFechaCorta } from '../../../utils/formatear'
+import { BotonDetalles } from '../../../components/botones/BotonDetalles'
+import { useRef } from 'react'
+import { COLORES_ETIQUETAS } from '../../../components/etiquetas/consts/etiquetas'
+
+const estados = {
+  completo: 'completo',
+  pendiente: 'pending',
+  enRevision: 'submitted'
+}
+
+const status = {
+  [estados.completo]: {
+    text: 'Completo',
+    color: COLORES_ETIQUETAS.verde
+  },
+  [estados.pendiente]: {
+    text: 'Pendiente',
+    color: COLORES_ETIQUETAS.celeste
+  },
+  [estados.enRevision]: {
+    text: 'En revisión',
+    color: COLORES_ETIQUETAS.amarillo
+  }
+}
+
+export function RenderExoneraciones () {
+  const headers = ['No.', 'Supervisor', 'Fecha', 'Taxista', 'Estado', '']
+
+  const exoneraciones = useRef(Array(10).fill(null).map((_, index) => ({
+    id: index + 1,
+    supervisor: `Supervisor ${index + 1}`,
+    fecha: new Date(),
+    taxista: `Taxista No. ${index + 1}`,
+    estado: Math.floor(Math.random() * 10) < 5 ? estados.enRevision : Math.floor(Math.random() * 10) === 5 ? estados.pendiente : estados.completo
+  }))).current
+
+  const loading = false
+
+  const claseCssRender = 'render-exoneraciones'
+  // const detallesExoneracionModal = useRef()
+
+  // const [currentDetallesModal, setCurrentDetallesModal] = useState(null)
+
+  return (
+    <TablaBase
+      headers={headers}
+      className={`${claseCssRender}`}
+    >
+      {
+        !loading &&
+        exoneraciones.map(({ id, supervisor, fecha, taxista, estado }, idx) => (
+          <ListStyleRow
+            key={id}
+          >
+            <li
+              title={id}
+            >
+              {id}
+            </li>
+            <li
+              title={supervisor}
+            >
+              {supervisor}
+            </li>
+            <li
+              title={formatearFechaCorta(fecha)}
+            >
+              {formatearFechaCorta(fecha)}
+            </li>
+            <li
+              title={taxista}
+            >
+              {taxista}
+            </li>
+            <li
+              title='Estado'
+            >
+              <Etiqueta
+                text={status[estado].text}
+                color={status[estado].color}
+                className='w-fit'
+              />
+            </li>
+
+            <li>
+              <BotonDetalles
+                onClick={() => {
+                  // setCurrentDetallesModal(exoneraciones[idx])
+                  // detallesExoneracionModal.current.showModal()
+
+                  alert('en construccion')
+                }}
+              >
+                Detalles
+              </BotonDetalles>
+            </li>
+          </ListStyleRow>
+        ))
+      }
+
+      {/* <DetallesExoneracion
+        refModal={detallesExoneracionModal}
+        detalles={currentDetallesModal}
+        onClose={() => setCurrentDetallesModal(null)}
+      /> */}
+    </TablaBase>
+  )
+}
