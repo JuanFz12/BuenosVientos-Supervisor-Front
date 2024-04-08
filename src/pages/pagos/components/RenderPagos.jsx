407fc3e982c9e79a9c0b4e944320f5a9d07ee08a
Patrick Diestra <patrickdiestra01@gmail.com>
2024-04-08 16:59:23 -0500

    maquetando la pagina de pagos

---
+++ src/pages/pagos/components/RenderPagos.jsx
@@ -0,0 +1,108 @@
+import './RenderPagos.css'
+
+import { useRef } from 'react'
+import { TablaBase } from '../../../components/tablas/TablaBase'
+import { COLORES_ETIQUETAS } from '../../../components/etiquetas/consts/etiquetas'
+import { ListStyleRow } from '../../../components/listStyle/ListStyleRow'
+import { formatearFechaCorta } from '../../../utils/formatear'
+import { formatearASoles } from '../../../utils/formatearASoles'
+import { Etiqueta } from '../../../components/etiquetas/Etiquetas'
+import { BotonDetalles } from '../../../components/botones/BotonDetalles'
+
+const estados = {
+  pendiente: 'Pending'
+}
+
+const status = {
+  [estados.pendiente]: {
+    text: 'Pendiente',
+    color: COLORES_ETIQUETAS.amarillo
+  }
+}
+
+export function RenderPagos () {
+  const headers = ['Operacion', 'Terminal', 'Fecha de pago', 'Banco', 'Deuda', 'Pagado', '']
+
+  const claseCssRender = 'render-pagos-cuotas'
+
+  const pagos = useRef(Array.from({ length: 10 }, (_, index) => ({
+    id: index + 1,
+    operacion: 'Operacion ' + Math.floor(Math.random() * 10000000) + index,
+    terminal: 'Terminal ' + index + 1,
+    fecha: Math.random() * 10 > 5 ? new Date() : estados.pendiente,
+    banco: Math.random() * 10 > 5 ? 'Interbank' : 'BBVA',
+    deuda: Math.floor(Math.random() * 9000) + index,
+    pagado: Math.floor(Math.random() * 9000) + index
+  }))).current
+
+  const loading = false
+
+  return (
+    <TablaBase
+      headers={headers}
+      className={claseCssRender}
+    >
+      {
+        !loading &&
+        pagos.map(({ id, operacion, terminal, fecha, banco, deuda, pagado }) => (
+          <ListStyleRow
+            key={id}
+          >
+            <li
+              title={operacion}
+            >
+              {operacion}
+            </li>
+
+            <li
+              title={terminal}
+            >
+              {terminal}
+            </li>
+
+            <li
+              title={
+                fecha === estados.pendiente
+                  ? status[fecha].text
+                  : formatearFechaCorta(fecha)
+              }
+            >
+              {
+                fecha === estados.pendiente
+                  ? (
+                    <Etiqueta
+                      text={status[fecha].text}
+                      color={status[fecha].color}
+                      className='w-fit'
+                    />
+                    )
+                  : formatearFechaCorta(fecha)
+              }
+            </li>
+            <li
+              title={banco}
+            >
+              {banco}
+            </li>
+            <li
+              title={formatearASoles({ numero: deuda, cero: true })}
+            >
+              {formatearASoles({ numero: deuda, cero: true })}
+            </li>
+            <li
+              title={formatearASoles({ numero: pagado, cero: true })}
+            >
+              {formatearASoles({ numero: pagado, cero: true })}
+            </li>
+
+            <li>
+              <BotonDetalles
+                onClick={() => alert('En desarrollo...')}
+              />
+            </li>
+          </ListStyleRow>
+        ))
+      }
+    </TablaBase>
+  )
+}
