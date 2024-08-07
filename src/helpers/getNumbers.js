1d065752fdea584e18f70c4df43d1e08532aae5c
Patrick Diestra <patrickdiestra01@gmail.com>
2024-08-07 11:11:31 -0500

    add(helper): Nuevo helper para obtener todos los números de un string y convertirlo en un solo numero

--- src/helpers/getNumbers.js
+++ src/helpers/getNumbers.js
@@ -1,15 +1,29 @@
 import { noNumbersRegex } from '../consts/regex'
 
-export function getNumbers (string, asString = false, returnNull = false) {
+/**
+ * @description: Función para obtener todos los números de una cadena y devolver un solo numero
+ */
+export function getNumbers ({ string, asString = false, returnNull = false, fixed }) {
   const returnIfVoid = returnNull ? null : asString ? '0' : 0
 
-  if (typeof string === 'number') return string // si es un numero retornarlo directamente
+  // si es un numero y no tiene fixed, retornarlo directamente
+  if (typeof string === 'number' && isNaN(fixed)) return string
 
   if (!string) return returnIfVoid
 
-  const numbers = string.replace(noNumbersRegex, '') // remplaza todo lo que no sea un numero por un ''
+  const stringToParse = string.toString()
+  const numbers = stringToParse.replace(noNumbersRegex, '') // remplaza todo lo que no sea un numero por un ''
 
   if (numbers === '') return returnIfVoid
 
-  return asString ? numbers : parseInt(numbers)
+  // Luego debería refactorizar
+  if (asString) {
+    if (isNaN(fixed)) return numbers
+
+    return parseFloat(numbers).toFixed(fixed)
+  } else {
+    if (isNaN(fixed)) return parseFloat(numbers)
+
+    return parseFloat(parseFloat(numbers).toFixed(fixed))
+  }
 }
